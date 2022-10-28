12555fe2a94152dbf63826a317a00ef7a8d3f3ab
sazzadi-r14 <sazzadi.cc9@gmail.com>
2022-10-27 22:42:46 -0700

    adding crypto

---
+++ Class_Lectures/unified_note.py
@@ -0,0 +1,82 @@
+#Class Notes
+
+for ch in s:
+    result = ch + ch
+
+lst[]
+lst.index()
+lst.append()
+
+#Alright let's start with string find
+
+string_var.find() #Anything in this function give out the index value of the particular strin gthat I am searching for. 
+>>> s = 'Python'
+>>> 
+>>> s.find('th')
+2
+>>> s.find('o')
+4
+>>> s.find('y')
+1
+>>> s.find('x')
+-1
+>>> s.find('N')
+-1
+>>> s.find('P')
+0
+
+#In depth view at the function
+
+s.find(target, start_index)
+
+>>> s = '[xyz['
+>>> s.find('[')      # find first [
+0
+>>> s.find('[', 1)   # start search at 1
+4
+
+# String Slicing
+>>> s = 'Python'
+>>> s[1:3]    # 1 .. UBNI
+'yt'
+>>> s[1:5]
+'ytho'
+>>> s[4:5]
+'o'
+>>> s[4:4]    # "not including" dominates
+''
+
+>>> s[:3]     # omit = from/to end
+'Pyt'
+>>> s[4:]
+'on'
+>>> s[4:999]  # too big = through the end
+'on'
+>>> s[:4]     # "perfect split" on 4
+'Pyth'
+>>> s[4:]
+'on'
+>>> s[:]      # the whole thing
+'Python'
+
+# Now here comes the part of case sensivity. 
+
+s.upper() s.lower() s.isupper() s.islowe>>> 'Kitten123'.upper()  # return with all chars in upper form
+'KITTEN123'
+>>> 'Kitten123'.lower()
+'kitten123'
+>>> 
+>>> 'a'.islower()
+True
+>>> 'A'.islower()
+False
+>>> 'A'.isupper()
+True
+>>> '@'.islower()
+False
+>>> 'a'.upper()
+'A'
+>>> 'A'.upper()
+'A'
+>>> '@'.upper()
+'@'
---
+++ crypto/crypto.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+
+"""
+Stanford CS106A Crypto Project
+"""
+
+import sys
+
+# provided ALPHABET constant - list of the regular alphabet
+# in lowercase. Refer to this simply as ALPHABET in your code.
+# This list should not be modified.
+ALPHABET = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
+def compute_slug(key):
+    """
+    Given a key string, compute and return the len-26 slug list for it.
+    >>> compute_slug('z')
+    ['z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y']
+    >>> compute_slug('Bananas!')
+    ['b', 'a', 'n', 's', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'o', 'p', 'q', 'r', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    >>> compute_slug('Life, Liberty, and')
+    ['l', 'i', 'f', 'e', 'b', 'r', 't', 'y', 'a', 'n', 'd', 'c', 'g', 'h', 'j', 'k', 'm', 'o', 'p', 'q', 's', 'u', 'v', 'w', 'x', 'z']
+    >>> compute_slug('Zounds!')
+    ['z', 'o', 'u', 'n', 'd', 's', 'a', 'b', 'c', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'p', 'q', 'r', 't', 'v', 'w', 'x', 'y']
+    """
+    pass
+
+
+def encrypt_char(source, slug, ch):
+    """
+    Given source and slug lists,
+    if the char ch is in source, return
+    its encrypted form. Otherwise return ch unchanged.
+    >>> # Compute 'z' slug, store it in a var named z_slug
+    >>> # and pass that in as the slug for the tests.
+    >>> z_slug = compute_slug('z')
+    >>> encrypt_char(ALPHABET, z_slug, 'A')
+    'Z'
+    >>> encrypt_char(ALPHABET, z_slug, 'n')
+    'm'
+    >>> encrypt_char(ALPHABET, z_slug, 'D')
+    'C'
+    >>> encrypt_char(ALPHABET, z_slug, '.')
+    '.'
+    >>> encrypt_char(ALPHABET, z_slug, ' ')
+    ' '
+    """
+    pass
+
+
+def encrypt_str(source, slug, s):
+    """
+    Given source and slug lists and string s,
+    return a version of s where every char
+    has been encrypted by source/slug.
+    >>> z_slug = compute_slug('z')
+    >>> encrypt_str(ALPHABET, z_slug, 'And like a thunderbolt he falls.')
+    'Zmc khjd z sgtmcdqanks gd ezkkr.'
+    """
+    pass
+
+
+def decrypt_str(source, slug, s):
+    """
+    Given source and slug lists and encrypted string s,
+    return the decrypted form of s.
+    >>> z_slug = compute_slug('z')
+    >>> decrypt_str(ALPHABET, z_slug, 'Zmc khjd z sgtmcdqanks gd ezkkr.')
+    'And like a thunderbolt he falls.'
+    """
+    pass
+
+
+def encrypt_file(filename, key):
+    """
+    Given filename and key, compute and
+    print the encrypted form of its lines.
+    """
+    pass
+
+
+def decrypt_file(filename, key):
+    """
+    Given filename and key, compute and
+    print the decrypted form of its lines.
+    """
+    pass
+
+
+def main():
+    args = sys.argv[1:]
+    # 2 command line argument patterns:
+    # -encrypt key filename
+    # -decrypt key filename
+    # Call encrypt_file() or decrypt_file() based on the args.
+    pass
+
+
+# Python boilerplate.
+if __name__ == '__main__':
+    main()
