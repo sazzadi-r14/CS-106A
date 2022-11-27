4978b185903896beb1ccef6513d0711339bf4383
sazzadi-r14 <sazzadi.cc9@gmail.com>
2022-11-26 21:30:19 -0800

    6a done

--- ghost/ghost.py
+++ ghost/ghost.py
@@ -15,15 +15,54 @@ from simpleimage import SimpleImage
 def pix_dist2(pix1, pix2):
     """
     Returns the square of the color distance between 2 pix tuples.
+    >>> pix_dist2((3, 4, 7), (14, 24, 29))
+    1005
+    >>> pix_dist2((4, 20, 9), (8, 24, 53))
+    1968
     """
-    pass
+
+    return (pix1[0] - pix2[0]) ** 2 + (pix1[1] - pix2[1]) ** 2 + (pix1[2] - pix2[2]) ** 2  # Implementing the eqn
+
+
+def avg_pixl(pixs):
+    """
+    Returns a tuple of the average rgb values from a list of pixels
+    >>> avg_pixl([(18, 18, 18), (20, 20, 20), (20, 20, 20), (20, 20, 20), (0, 2, 0), (1, 0, 1)])
+    (13.166666666666666, 13.333333333333334, 13.166666666666666)
+    >>> avg_pixl([(14, 23, 20), (24, 18, 53), (13, 18, 29), (4, 7, 33), (42, 47, 50), (80, 5, 9)])
+    (29.5, 19.666666666666668, 32.333333333333336)
+    """
+    # Creating a variable get the sum of all the colors.
+    red = 0
+    green = 0
+    blue = 0
+    total = len(pixs)
+    for pixel in pixs:
+        red += pixel[0]
+        green += pixel[1]
+        blue += pixel[2]
+
+    avg = (red / total, green / total, blue / total)  # Calculates the average of all values, and stores it in a tuple.
+    return avg
 
 
 def best_pix(pixs):
     """
     Given a list of 1 or more pix, returns the best pix.
+
+    >>> best_pix([(1, 1, 1), (1, 1, 1), (28, 28, 28)])
+    (1, 1, 1)
+    >>> best_pix([(14, 24, 9), (20, 23, 24), (29, 53, 24)])
+    (20, 23, 24)
+    >>> best_pix([(59, 39, 42), (38, 54, 6), (9, 18, 33)])
+    (38, 54, 6)
+    >>> best_pix([(59, 39, 42), (10, 18, 32), (9, 18, 33)])
+    (10, 18, 32)
+    >>> best_pix([(36, 54, 7), (38, 54, 6), (9, 18, 33)])
+    (36, 54, 7)
     """
-    pass
+    avg = avg_pixl(pixs)
+    return min(pixs, key=lambda pixel: pix_dist2(pixel, avg))  # Returns the closest tuple to average.
 
 
 def good_apple_pix(pixs):
@@ -33,7 +72,23 @@ def good_apple_pix(pixs):
     >>> good_apple_pix([(18, 18, 18), (20, 20, 20), (20, 20, 20), (20, 20, 20), (0, 2, 0), (1, 0, 1)])
     (20, 20, 20)
     """
-    pass
+    avg = avg_pixl(pixs)
+    srt_lst = sorted(pixs, key=lambda pixel: pix_dist2(pixel, avg))
+    return best_pix(srt_lst[:len(srt_lst) // 2])  # Uses the best_pix function for good half.
+
+
+def lst_pixl(images, x, y, mode):
+    """
+    This helper function takes a list of images and returns the best pixel tuple of a particular co-ordinate.
+    """
+    pixel_lst = []
+    for image in images:
+        pixel_lst.append(image.get_pix(x, y))  # This builds a list of tuples for the specific pixel of all the photos.
+    # Instead of putting the argument in solve, putting it here made it easier to execute this code.
+    if mode == None:
+        return best_pix(pixel_lst)
+    elif mode == '-good':
+        return good_apple_pix(pixel_lst)
 
 
 def solve(images, mode):
@@ -41,10 +96,18 @@ def solve(images, mode):
     Given a list of image objects and mode,
     compute and show a Ghost solution image based on these images.
     Mode will be None or '-good'.
-    There will be at least 3 images and they will all be
+    There will be at least 3 images, and they will all be
     the same size.
     """
-    pass
+    # Classic blank image building algorithm, just using the tuple technique in less lines.
+    width = images[0].width
+    height = images[0].height
+    solution = SimpleImage.blank(width, height)
+    for y in range(height):
+        for x in range(width):
+            pix = lst_pixl(images, x, y, mode)
+            solution.set_pix(x, y, pix)
+    solution.show()
 
 
 def jpgs_in_dir(dir):
